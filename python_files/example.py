95bd6524e30edfa9faa1986877664e16cc789b1d
MacBook <71572963+guoyu24@users.noreply.github.com>
2023-12-18 15:22:26 +0000

    changed dataset

--- python_files/example.py
+++ python_files/example.py
@@ -85,12 +85,12 @@ class ScriptArguments:
     )
 
 
-def extract_anthropic_prompt(prompt_and_response):
-    """Extract the anthropic prompt from a prompt and response pair."""
-    search_term = "\n\nAssistant:"
-    search_term_idx = prompt_and_response.rfind(search_term)
-    assert search_term_idx != -1, f"Prompt and response does not contain '{search_term}'"
-    return prompt_and_response[: search_term_idx + len(search_term)]
+# def extract_anthropic_prompt(prompt_and_response):
+#     """Extract the anthropic prompt from a prompt and response pair."""
+#     search_term = "\n\nAssistant:"
+#     search_term_idx = prompt_and_response.rfind(search_term)
+#     assert search_term_idx != -1, f"Prompt and response does not contain '{search_term}'"
+#     return prompt_and_response[: search_term_idx + len(search_term)]
 
 
 def get_hh(split: str, sanity_check: bool = False, silent: bool = False, cache_dir: str = None) -> Dataset:
@@ -107,20 +107,21 @@ def get_hh(split: str, sanity_check: bool = False, silent: bool = False, cache_d
       \n\nHuman: <prompt>\n\nAssistant:
     Multiple turns are allowed, but the prompt should always start with \n\nHuman: and end with \n\nAssistant:.
     """
-    dataset = load_dataset("Anthropic/hh-rlhf", split=split, cache_dir=cache_dir)
+    # dataset = load_dataset("Anthropic/hh-rlhf", split=split, cache_dir=cache_dir)
+    dataset = load_dataset("Dahoas/full-hh-rlhf", split=split, cache_dir=cache_dir)
     if sanity_check:
         dataset = dataset.select(range(min(len(dataset), 1000)))
 
-    def split_prompt_and_responses(sample) -> Dict[str, str]:
-        prompt = extract_anthropic_prompt(sample["chosen"])
-        return {
-            "prompt": prompt,
-            "chosen": sample["chosen"][len(prompt) :],
-            "rejected": sample["rejected"][len(prompt) :],
-        }
+    # def split_prompt_and_responses(sample) -> Dict[str, str]:
+    #     prompt = extract_anthropic_prompt(sample["chosen"])
+    #     return {
+    #         "prompt": prompt,
+    #         "chosen": sample["chosen"][len(prompt) :],
+    #         "rejected": sample["rejected"][len(prompt) :],
+    #     }
 
-    return dataset.map(split_prompt_and_responses)
-
+    # return dataset.map(split_prompt_and_responses)
+    return dataset
 
 if __name__ == "__main__":
     parser = HfArgumentParser(ScriptArguments)
